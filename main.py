34a4d009421c59cfdb403a11f4e3fc311dad9501
Walter R <wroosa111@gmail.com>
2025-08-25 12:29:53 -0400

    Add game loop and ability to exit the game

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import * 
 
 def main():
     print('Starting Asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
+
+    # Game Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill('black')
+
+        # Render
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
